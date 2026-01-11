1861a39f3d80a1249fcf2b26b1706d6aede02b76
Jonathan Ballard <asgardschosen@gmail.com>
2026-01-11 14:37:26 -0800

    Create function stubs for accounts views.py

---
+++ osr/accounts/utils.py
@@ -0,0 +1,23 @@
+
+# utils.py
+
+def validate_login():
+    pass
+
+def check_login_password():
+    pass
+
+def check_user_exists():
+    pass
+
+
+
+def validate_signup():
+    pass
+
+def check_valid_signup_password():
+    pass
+
+def check_valid_signup_email():
+    pass
+
--- osr/accounts/views.py
+++ osr/accounts/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# GET login
+# POST login
+# GET signup
+# POST signup
+
+def get_login():
+    pass
+
+def post_login():
+    pass
+
+def get_signup():
+    pass
+
+def post_signup():
+    pass
