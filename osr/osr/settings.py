1848317e3419519d7ea94380b38c9e7ca842d9f2
Jonathan Ballard <asgardschosen@gmail.com>
2026-01-23 20:00:44 -0800

    Add CORS protection for future React usage
    Added django-cors-headers. Edited settings.py to include configuration for CORS protection

--- osr/osr/settings.py
+++ osr/osr/settings.py
@@ -57,11 +57,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
+    
     'accounts',
     'recipes',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+        
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +132,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # for local development
+    "http://localhost:8000", 
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
 ]
 
 
