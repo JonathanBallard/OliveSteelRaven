4dbf8246c54256fdd947314a6b6516f86d7315c4
Jonathan Ballard <asgardschosen@gmail.com>
2026-02-14 22:56:11 -0800

    Settings change

--- osr/osr/settings.py
+++ osr/osr/settings.py
@@ -240,8 +240,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'osrdb'),
-        'USER': os.getenv('DB_SUPERUSER'),
-        'PASSWORD': os.getenv('DB_SUPERPASS'),
+        'USER': os.getenv('DB_PROD_USER'),
+        'PASSWORD': os.getenv('DB_PROD_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),  # Or the host IP if not local
         'PORT': os.getenv('DB_PORT', ''),           # PostgreSQL default port is 5432. Leaving empty uses the default.
     }
